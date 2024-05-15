bce7f02b5d15daadd2a9ef205605198b92ee2c6f
jesicangreni <iss22018@students.del.ac.id>
2024-05-15 15:41:10 +0700

    coba submit 2
    Kelupaan memasukkan codes untuk model Dorm dan Student

--- src/main/java/pbo/f01/model/Dorm.java
+++ src/main/java/pbo/f01/model/Dorm.java
@@ -1,3 +1,14 @@
+package pbo.f01.model;
+
 public class Dorm {
-    
+    private String Name;
+    private String Gender;
+    private int Capacity;
+
+    public Dorm (String Name, String Gender, int Capacity){
+        this.Name = Name;
+        this.Gender = Gender;
+        this.Capacity = Capacity;
+    }
+
 }
--- src/main/java/pbo/f01/model/Student.java
+++ src/main/java/pbo/f01/model/Student.java
@@ -1,3 +1,16 @@
+package pbo.f01.model;
+
 public class Student {
-    
+    private String Id;
+    private String Name;
+    private String Gender;
+    private int entranceYear;
+
+public Student (String Id, String Name, String Gender, int entranceYear){
+    this.Id = Id;
+    this.Name = Name;
+    this.Gender = Gender;
+    this.entranceYear = entranceYear;
+}
+
 }
